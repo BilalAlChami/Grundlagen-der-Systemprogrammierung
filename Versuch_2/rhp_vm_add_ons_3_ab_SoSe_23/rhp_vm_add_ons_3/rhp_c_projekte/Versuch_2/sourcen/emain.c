cd390a4d8e17a4b894d756bcd1b953cb72df158a
BilalAlChami <143915850+BilalAlChami@users.noreply.github.com>
2025-05-03 05:26:59 +0200

    Add files via upload

---
+++ Versuch_2/rhp_vm_add_ons_3_ab_SoSe_23/rhp_vm_add_ons_3/rhp_c_projekte/Versuch_2/sourcen/emain.c
@@ -0,0 +1,64 @@
+﻿#include "simuc.h"
+#include "io_treiber.h"
+
+
+// Die Verwendung der folgenden Zustandsnamen ist verbindlich
+typedef enum {hoch, runter, steht} STATE;
+
+// Die folgenden Defines muessen sinnvoll genutzt werden
+// Einige Defines zum leichteren Wiederfinden
+#define BIT_POS_IST_OBEN	0
+#define BIT_POS_IST_UNTEN	1
+#define BIT_POS_NACH_OBEN	4
+#define BIT_POS_NACH_UNTEN	5
+
+#define BIT_POS_MOTOR_AN	0
+#define BIT_POS_FAHRE_NACH_OBEN	1
+#define BIT_POS_FAHRE_NACH_UNTEN	2
+
+
+// Hier die Steuerungsfunktion definieren
+// ...
+
+void emain(void* arg) 
+{
+	// Hier alle benoetigten Variablen deklarieren
+	// ...
+
+
+	INIT_BM_WITH_REGISTER_UI; // Hier unbedingt einen Break-Point setzen !!!
+
+	
+	// Hier die Treiberfunktionen aufrufen und testen (Aufgabe 1)
+	// ...
+
+
+
+
+
+	// Ab hier beginnt die Endlosschleife fuer den Automaten (Aufgabe 2)
+	while(1) {
+
+		SYNC_SIM; 
+
+		// Hier die Eingabesignale einlesen
+		// ...
+
+		
+
+		// Hier die Steuerungsfunktion aufrufen
+		// ...
+
+
+
+
+		// Hier die Ausgabesignale ausgeben
+		// ...
+
+
+	} // while(1)..
+
+	
+}
+
+  
---
+++ Versuch_2/rhp_vm_add_ons_3_ab_SoSe_23/rhp_vm_add_ons_3/rhp_c_projekte/Versuch_2/sourcen/io_teiber.c
@@ -0,0 +1,69 @@
+﻿#include "simuc.h"
+#include "io_treiber.h"
+
+#include <stdio.h>
+
+typedef struct BHandle_Data {
+	BYTE Board_allocated;	// 1=allocated, 0=free
+	BYTE Port_A_Direction;	// 1=Output, 0=Input
+	BYTE Port_B_Direction;
+	BYTE Port_C_Direction;
+	BYTE Port_D_Direction;
+} BHandle;
+
+
+
+DSCB GlobalBoardHandle = NULL;  // Dies ist ein Zeiger (siehe typedef BHandle* DSCB in io_treinber.h),
+                                // der noch auf einen gueltigen Speicherbereich "gebogen" werden muss.
+                                // Dieser Speicherbereich muss dann in Init() mittels calloc() allokiert
+                                // werden.
+
+
+
+BYTE Init(DSCB* Zeiger_auf_BoardHandle, unsigned long int Steuerwort) {
+
+    DSCB Lokales_BoardHandle;   // Es wird empfohlen innerhalb dieser Funktion mit diesem
+                                // lokalen BoardHandle zu arbeiten. Ein Zugriff auf die
+                                // Stukturkomponenten ist dann ganz normal, z.B. mittels
+                                // Lokales_BoardHandle->Borad_allocated, moeglich.
+
+
+    if (*Zeiger_auf_BoardHandle == NULL){	
+        // Allokieren des Speichersbereiches und "biegen"  des lokalen Boadhandle auf diesen
+        // Speicherbereich.
+        Lokales_BoardHandle = (DSCB) calloc(1, sizeof(BHandle));
+
+        // Auch den per Call-By-Reference uebergebenen Zeiger auf den reservierten Speicherbereich "biegen"
+        *Zeiger_auf_BoardHandle = Lokales_BoardHandle;
+    } else {	
+        Lokales_BoardHandle = *Zeiger_auf_BoardHandle; // Damit bei Mehrfachaufrufen von Init() das "Lokales_BoardHande" richtig initialisiert wird
+    }
+
+    // Ab hier nur noch mit dem lokalen BoardHande arbeiten
+    
+	// Mit Leben fuellen
+	
+	return 0;
+}
+
+BYTE InputByte(DSCB BoardHandle, BYTE Port, BYTE *DigitalValue) {
+    
+	// Mit Leben fuellen
+
+	return 0;
+}
+
+BYTE OutputByte(DSCB BoardHandle, BYTE Port, BYTE DigitalValue) {
+    
+	// Mit Leben fuellen
+
+	return 0;
+}
+
+BYTE Free(DSCB BoardHandle) {
+    
+	// Mit Leben fuellen
+	
+	return 0;
+}
+
