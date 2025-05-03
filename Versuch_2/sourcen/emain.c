de7e122005324b5eaef158fafaf30f4681278e10
BilalAlChami <143915850+BilalAlChami@users.noreply.github.com>
2025-05-03 05:26:12 +0200

    Add files via upload

---
+++ Versuch_2/sourcen/emain.c
@@ -0,0 +1,156 @@
+﻿#include "simuc.h"
+        #include "io_treiber.h"
+
+
+// Die Verwendung der folgenden Zustandsnamen ist verbindlich
+        typedef enum {hoch, runter, steht} STATE;
+
+// Die folgenden Defines muessen sinnvoll genutzt werden
+// Einige Defines zum leichteren Wiederfinden
+        #define BIT_POS_IST_OBEN	0
+        #define BIT_POS_IST_UNTEN	1
+        #define BIT_POS_NACH_OBEN	4
+        #define BIT_POS_NACH_UNTEN	5
+
+        #define BIT_POS_MOTOR_AN	0
+        #define BIT_POS_FAHRE_NACH_OBEN	1
+        #define BIT_POS_FAHRE_NACH_UNTEN	2
+
+        static long int startpos;
+        static long int endpos;
+        static long int aktpos;
+
+        void stgfunc (unsigned short int HPL, unsigned short int HPR, unsigned short int T1, unsigned short int T2,
+        unsigned short int* M_Re,unsigned short int *M_Li,unsigned short int* M_An, STATE* pstate,long int aktpos ) {
+        switch (*pstate) {
+
+        case steht:
+        *M_Li = 0;
+        *M_An=0;
+        *M_Re=0;
+        if(T1==1)
+        {
+        *pstate=hoch;
+        }
+        else if(T2==1){
+        *pstate=runter;
+        }
+
+        break;
+        case hoch:
+        *M_Li=1;*M_Re=0;*M_An=1;
+        startpos=aktpos;
+        endpos = (startpos - 60000) % 65535;
+        if(endpos<0)
+        {
+        endpos=endpos+65535;
+        }
+        if(T1==1)
+        {
+        *pstate=hoch;
+        }
+
+
+        break;
+        case runter:
+        *M_Li=0;*M_Re=1;*M_An=1;
+        startpos=aktpos;
+        endpos = (startpos + 60000) % 65535;
+        if(T1==1){
+        *pstate=runter;
+        }
+
+        break;
+
+        default: *pstate=steht;
+        }
+}
+
+        void emain(void* arg)
+        {
+        // 2) Definition des Startzustandes
+        STATE pstate;
+        // Hier alle benoetigten Variablen deklarieren
+        // ...
+        unsigned short int HPL; unsigned short int HPR; unsigned short int T1; unsigned short int T2;
+
+        //Variable für die Ausgabesignale (Aufgabe 2)
+        unsigned short int M_Re;unsigned short int M_Li;unsigned short int M_An;
+        BYTE output;
+        BYTE test1,test2,test3,test4,test5,test6,x;
+        test5=15;
+
+        INIT_BM_WITH_REGISTER_UI; // Hier unbedingt einen Break-Point setzen !!!
+
+
+        // Hier die Treiberfunktionen aufrufen und testen (Aufgabe 1)
+        // ...
+        test1=Init(&GlobalBoardHandle, 0x9A);
+        test2=InputByte(GlobalBoardHandle,2,&test4);
+        test3=OutputByte(GlobalBoardHandle,3,test5);
+        test6=Free(GlobalBoardHandle);
+
+
+        pstate=steht;
+        Init(&GlobalBoardHandle,0x9A );// Port D als Ausgang und Port A,B,C als Eingänge
+
+        // Ab hier beginnt die Endlosschleife fuer den Automaten (Aufgabe 2)
+        while(1) {
+
+        //    Hier alle benoetigten Variablen deklarieren
+        // Variable fuer Aufgabe 1
+
+
+        SYNC_SIM;
+
+
+        // Hier die Treiberfunktionen aufrufen und testen (Aufgabe 1)
+        // ...
+        //test1=Init(GlobalBoardHandle, 0x80);
+        // test2=InputByte(GlobalBoardHandle,1,33);
+        //) test3=OutputByte(GlobalBoardHandle,1, 22);
+
+
+
+
+        // 1) Hardware konfigurieren
+        //Free(GlobalBoardHandle);
+
+
+
+
+
+        //3)Unendliche Schleife
+        // Ab hier beginnt die Endlosschleife fuer den Automaten (Aufgabe 2)
+
+
+        InputByte(GlobalBoardHandle,PC,&x);
+        // Hier die Eingabesignale einlesen
+
+
+        HPL=(x>>BIT_POS_IST_OBEN)&0x01;
+        HPR=(x>>BIT_POS_IST_UNTEN)&0x01;
+        T1=(x>>BIT_POS_NACH_OBEN)&0x01;
+        T2=(x>>BIT_POS_NACH_UNTEN)&0x01;
+
+        InputByte(GlobalBoardHandle,PA,&x);
+        BYTE a=x;
+        InputByte(GlobalBoardHandle,PB,&x);
+        BYTE b=x;
+        aktpos=(b<<8)|a;
+
+
+        // Hier die Steuerungsfunktion aufrufen
+        stgfunc(HPL,HPR,T1,T2,&M_Re,&M_Li,&M_An,&pstate,aktpos );
+
+        // Hier die Ausgabesignale ausgeben
+
+        output=output&~((1<<BIT_POS_MOTOR_AN)|(1<<BIT_POS_FAHRE_NACH_OBEN)|(1<<BIT_POS_FAHRE_NACH_UNTEN));
+        output=output|((M_Re)<<BIT_POS_FAHRE_NACH_UNTEN)|((M_Li)<<BIT_POS_FAHRE_NACH_OBEN)|((M_An)<<BIT_POS_MOTOR_AN);
+
+        OutputByte(GlobalBoardHandle,PD,output);
+
+        }
+
+
+        }
---
+++ Versuch_2/sourcen/io_teiber.c
@@ -0,0 +1,191 @@
+﻿#include "simuc.h"
+#include "io_treiber.h"
+
+#include <stdio.h>
+
+typedef struct BHandle_Data {
+	BYTE Board_allocated;	// 1=allocated, 0=free
+	BYTE Port_A_Direction;	// 1=Output, 0=Input
+	BYTE Port_B_Direction;
+	BYTE Port_C_Direction;
+	BYTE Port_D_Direction;
+} BHandle;
+
+typedef BHandle* DSCB;
+
+DSCB GlobalBoardHandle = NULL;  // Dies ist ein Zeiger (siehe typedef BHandle* DSCB in io_treinber.h),
+                                // der noch auf einen gueltigen Speicherbereich "gebogen" werden muss.
+                                // Dieser Speicherbereich muss dann in Init() mittels calloc() allokiert
+                                // werden.
+
+
+
+BYTE Init(DSCB* Zeiger_auf_BoardHandle, unsigned long int Steuerwort)
+{
+    DSCB Lokales_BoardHandle;
+
+    if (*Zeiger_auf_BoardHandle == NULL) {
+        // Allokieren des Speicherbereichs und "Biegen" des lokalen BoardHandles darauf
+        Lokales_BoardHandle = (DSCB)calloc(1, sizeof(BHandle));
+        if (Lokales_BoardHandle == NULL) {
+            // Fehler beim Allokieren des Speicherbereichs
+            return 1;
+        }
+        *Zeiger_auf_BoardHandle = Lokales_BoardHandle; // "Biegen" des übergebenen Zeigers
+    } else {
+        Lokales_BoardHandle = *Zeiger_auf_BoardHandle;
+    }
+
+    // Ab hier nur noch mit dem lokalen BoardHandle arbeiten
+
+    // Verwaltungsdaten entsprechend dem Steuerwort setzen
+    switch (Steuerwort) {
+        case 0x92:
+            Lokales_BoardHandle->Board_allocated = 1;
+            Lokales_BoardHandle->Port_A_Direction = 0;
+            Lokales_BoardHandle->Port_D_Direction = 1;
+            Lokales_BoardHandle->Port_C_Direction = 1;
+            Lokales_BoardHandle->Port_B_Direction = 0;
+            break;
+        case 0x93:
+            Lokales_BoardHandle->Board_allocated = 1;
+            Lokales_BoardHandle->Port_A_Direction = 0;
+            Lokales_BoardHandle->Port_D_Direction = 0;
+            Lokales_BoardHandle->Port_C_Direction = 1;
+            Lokales_BoardHandle->Port_B_Direction = 0;
+            break;
+        case 0x9A:
+            Lokales_BoardHandle->Board_allocated = 1;
+            Lokales_BoardHandle->Port_A_Direction = 0;
+            Lokales_BoardHandle->Port_D_Direction = 1;
+            Lokales_BoardHandle->Port_C_Direction = 0;
+            Lokales_BoardHandle->Port_B_Direction = 0;
+            break;
+        case 0x9B:
+            Lokales_BoardHandle->Board_allocated = 1;
+            Lokales_BoardHandle->Port_A_Direction = 0;
+            Lokales_BoardHandle->Port_D_Direction = 0;
+            Lokales_BoardHandle->Port_C_Direction = 0;
+            Lokales_BoardHandle->Port_B_Direction = 0;
+            break;
+        default:
+            // Ungültiges Steuerwort
+            return 2;
+    }
+
+    return 0;
+}
+
+BYTE InputByte(DSCB BoardHandle, BYTE Port, BYTE *DigitalValue) {
+    
+	// Mit Leben fuellen
+
+    if(BoardHandle->Board_allocated==0)
+        return 1;
+     switch(Port) {
+        case 3 :
+            if(BoardHandle->Port_D_Direction==0){
+                *DigitalValue=io_in16(IN1)>>8;
+                return 0;
+            }
+            return 3;
+            break;
+
+        case 1:
+
+            if(BoardHandle->Port_B_Direction==0){
+                *DigitalValue=io_in16(IN0)>>8;
+            return 0;
+            }
+            else
+                return 3;
+            break;
+
+        case 0   :
+            if(BoardHandle->Port_A_Direction==0){
+                *DigitalValue=io_in16(IN0)&0x00FF;
+            return 0;
+            }
+            else
+                return 3;
+            break;
+        case  2 :
+            if(BoardHandle->Port_C_Direction==0){
+                *DigitalValue=io_in16(IN1)&0x00FF;
+            return 0;
+            }
+            else
+                return 3;
+
+            default :
+                return 2;
+        }
+    }
+
+BYTE OutputByte(DSCB BoardHandle, BYTE Port, BYTE DigitalValue) {
+    
+	// Mit Leben fuellen
+    unsigned short out1;
+       unsigned short out2;
+       if(BoardHandle->Board_allocated==0)
+            return 1;
+       switch(Port){
+           case 3:
+               if(BoardHandle->Port_D_Direction==1){
+                   out1=(io_in16(IN1));
+                   out1=out1& 0x00FF;//die Bits des Ports C behalten und der Port D auf 0 setzen
+                   out2=(DigitalValue<<8); // binary um 8 Stellen nach Links verschieben und mit den Output verodern ergibt in port C die binäre Wert der Digitalvalue und Port D bleibt unverändert
+                   io_out16(OUT1,out2|out1);
+                   return 0;
+               }
+               else {
+                   return 3;
+               }
+           case 1:
+               if(BoardHandle->Port_B_Direction==1){
+                   out1=(io_in16(OUT0)|0x00FF);//die Bits des Ports A behalten und der Port B auf 0 setzen
+                   out2=((DigitalValue<<8)|out1);// binary um 8 Stellen nach Links verschieben und mit den Output verodern ergibt in port A die binäre Wert der Digitalvalue und Port Bbleibt unverändert
+                   io_out16(OUT0,out2);
+                   return 0;
+               }
+               else {
+                   return 3;
+               }
+           case 2:
+               if(BoardHandle->Port_C_Direction==1){
+                   out1=(io_in16(OUT1)&0xFF00); //die Bits des Ports D behalten und der Port C auf 0 setzen
+                   out2=(DigitalValue | out1 );      // binary mit den Output verodern ergibt in port C die binäre Wert der Digitalvalue und Port D bleibt unverändert
+                   io_out16(OUT1,out2);
+                   return 0;
+               }
+               else{
+                   return 3;
+               }
+           case 0:
+               if(BoardHandle->Port_A_Direction==1){
+                   out1=(io_in16(OUT0)& 0xFF00); //die Bits des Ports B behalten und der Port A auf 0 setzen
+                   out2=(DigitalValue | out1 );       // binary mit den Output verodern ergibt in port A die binäre Wert der Digitalvalue und Port B bleibt unverändert
+                   io_out16(OUT0,out2);
+                   return 0;
+               }
+               else{
+                   return 3;
+               }
+
+               default :
+                   return 2;
+
+       }
+   }
+
+
+BYTE Free(DSCB BoardHandle) {
+    
+	// Mit Leben fuellen
+
+    if(BoardHandle->Board_allocated==1){
+        BoardHandle->Board_allocated=0;
+        return 0;}
+    else
+        return 1;
+}
